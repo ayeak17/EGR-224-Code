6072c05bdc85ef4a3a514e2275dd41bf972988e0
ayeak17 <ayeak17@gmail.com>
2026-01-20 19:29:28 -0500

    Initial structure

--- 224-hw1-to_hex.c
+++ 224-hw1-to_hex.c
@@ -1,1 +1,13 @@
-# 
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    printf("Enter a base 10 integer between 0 and 65535:\n");
+    int decimal = 0;
+    scanf("%d", &decimal);
+
+    
+
+    return 0;
+}
